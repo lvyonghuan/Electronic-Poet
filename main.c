9a6627ba5c59f8942f6a85e68c2af798cc707463
lvyonghuan <110887355+lvyonghuan@users.noreply.github.com>
2023-05-26 00:03:37 +0800

    updata store
    增加了存储功能
    但是字符编码问题让人头痛

--- main.c
+++ main.c
@@ -7,18 +7,6 @@
 #include <string.h>
 #include <time.h>
 
-//函数区
-int Input(); //输入处理
-int Start(); //开始生成流程
-
-char *SelectSentence();//选择句式
-char *SelectNoun();//选择名词
-char *SelectVerb();//选择动词
-char *SelectAdj();//选择形容词
-int SpliceSentence(char *sentence);//拼接句子并打印
-
-int ReadFile(); //读取字典内容，错误返回1
-void EndProgram();//释放内存（然而操作系统好像会自动回收）
 
 //结构体区
 
@@ -44,6 +32,11 @@ typedef struct Sentence {
     struct Sentence *next;
 } sentence;
 
+typedef struct Poetry{
+    char *data;
+    struct Poetry *next;
+}poetry;
+
 //全局变量区（为什么用全局变量？因为本来就是全局要用的变量，传过来传过去没意思）
 
 int line[4] = {0};//1.名词行数；2.动词行数；3.形容词行数；4.句式行数
@@ -51,6 +44,23 @@ noun *noun_head = NULL;
 verb *verb_head = NULL;
 adjective *adj_head = NULL;
 sentence *sentence_head = NULL;
+
+//函数区
+int Input(); //输入处理
+int Start(); //开始生成流程
+
+char *SelectSentence();//选择句式
+char *SelectNoun();//选择名词
+char *SelectVerb();//选择动词
+char *SelectAdj();//选择形容词
+int SpliceSentence(char *sentence,int isFailStore,poetry *tail);//拼接句子并打印
+poetry *InitPoetryStore();//初始化store链表
+void StoreSentence(char *str,poetry *tail);//储存写的诗句
+int WriteFile(poetry *head);//把诗句写入进文件
+void FreePoetry(poetry *head);//释放存储诗句的内存
+
+int ReadFile(); //读取字典内容，错误返回1
+void EndProgram();//释放内存（然而操作系统好像会自动回收）
 
 int main() {
     int err = ReadFile();//先读取外存字典内容进入内存
@@ -135,6 +145,7 @@ int ReadFile() {
                     break;
             }
         }
+		fclose(file);//关闭文件
     }
     return 0;
 }
@@ -170,19 +181,39 @@ int Start() {
             return 1;//程序异常状态退出
         }
         srand(time(NULL));//设置随机数种子
+        //初始化存储模块
+        int isFailStore=0;
+        poetry *head=InitPoetryStore();
+        poetry *tail=head;
+        if(head==NULL){
+            isFailStore=1;
+            printf("初始化储存进程失败，储存诗句功能不可用。\n");
+        }
+		head->data=NULL;
+        head->next=NULL;
         printf("电子诗人咏唱中：\n");
         for (int i = 0; i < l; i++) {
             char *sentence = SelectSentence();
             if (sentence == NULL) {
                 return 1;
             }
-            int err = SpliceSentence(sentence);
+            int err = SpliceSentence(sentence,isFailStore,tail);
             if (err) {
                 return 1;
             }
         }
-        printf("是否继续生成？是请输入Y，否输入除Y的任意键结束生成。\n");
         char flag;
+        if(!isFailStore){
+            printf("是否储存诗句？是请按Y，否输入除Y的任意键：");
+            getchar();
+            scanf("%c",&flag);
+            if(flag=='Y'){
+				WriteFile(head);
+            }else{
+				FreePoetry(head);
+			}
+        }
+        printf("是否继续生成？是请输入Y，否输入除Y的任意键结束生成：");
         getchar();
         scanf("%c",&flag);
         if(flag!='Y'){
@@ -214,7 +245,8 @@ char *SelectSentence(){
     return NULL;
 }
 
-int SpliceSentence(char *sentence){
+int SpliceSentence(char *sentence,int isFailStore,poetry *tail){
+	char* str = (char*)malloc(sizeof(char) * 50);
     while (*sentence!='\0'){
         if(*sentence=='n'){
             char *n=SelectNoun();
@@ -223,6 +255,7 @@ int SpliceSentence(char *sentence){
                 return 1;
             }
             printf("%s",n);
+            strcat(str,n);
         }else if(*sentence=='v'){
             char *v=SelectVerb();
             if(v==NULL){
@@ -230,6 +263,7 @@ int SpliceSentence(char *sentence){
                 return 1;
             }
             printf("%s",v);
+            strcat(str,v);
         }else if(*sentence=='a'){
             char *a=SelectAdj();
             if(a==NULL){
@@ -237,12 +271,18 @@ int SpliceSentence(char *sentence){
                 return 1;
             }
             printf("%s",a);
+            strcat(str,a);
         }else{
             printf("%c",*sentence);
+            char temp[2] = {*sentence, '\0'};
+            strcat(str, temp);
         }
         *sentence++;
     }
     printf("\n");
+    if(!isFailStore){
+        StoreSentence(str,tail);
+    }
     return 0;
 }
 
@@ -277,4 +317,46 @@ char *SelectAdj(){
         }
     }
     return NULL;
+}
+
+poetry *InitPoetryStore(){
+    poetry *head=(poetry *)malloc(sizeof (poetry));
+    return head;
+}
+
+void StoreSentence(char *str,poetry *tail){
+    poetry *p=(poetry *) malloc(sizeof (poetry));
+    p->data= strdup(str);
+    p->next=NULL;
+    tail->next=p;
+    tail=p;
+}
+
+int WriteFile(poetry *head){
+	printf("请输入诗的标题：");
+	char title[1000];
+	scanf("%s",title);
+	char name[1000]="collection/";
+	strcat(name,title);
+	strcat(name,".txt");
+	FILE *file=fopen(name,"w");
+	if(file==NULL){
+		printf("文件创建失败！\n");
+		return 1;
+	}
+	for(poetry *i=head->next;i!=NULL;i=i->next){
+		fprintf(file,"%s\r\n",i->data);
+	}
+	FreePoetry(head);//释放内存
+	fclose(file);
+	return 0;
+}
+
+void FreePoetry(poetry *head){
+	for(poetry *i=head;i!=NULL;){
+		poetry *temp=i->next;
+		free(i->data);
+		free(i);
+		i=temp;
+	}
 }
